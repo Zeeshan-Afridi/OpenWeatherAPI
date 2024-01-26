968f8a5b69111d6617b49519d2265d93eeda7ba5
zeeshan-afridi <zeeafridi798@gmail.com>
2024-01-26 15:18:33 +0500

    Modified and properly documented code

--- script.py
+++ script.py
@@ -1,29 +1,24 @@
 import requests
 import json
 
-API_key = "2a8f13773d1ec72d8c99d504f073861d"
+# add your openweahter api key here
+API_key = API
 city_name = "Peshawar"
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=metric"
 
-# url = f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_key}"
-
-
+#access the url
 respone = requests.get(url)
 
+# get the data from the respose in json formate
 data = respone.json()
 
-
+# access targeted attributes and store it in variables
 description = data['weather'][0]["description"]
 humidity = data["main"]["humidity"]
 temp_min = data['main']['temp_min']
 temp_max = data['main']["temp_max"]
 temp = data['main']["temp"]
 
+# print the attributes
 print(f"Maximum Temperature is: {temp_max} \nMinimum Temperature is: {temp_min} \nCurrent Temperature is: {temp}" +
       f"\nCurrent Temperature Description is: {description} \nCurrent Temperature Humidity is: {humidity}")
-
-#Prettify the JSON data by specifying indent level
-# formatted_data = json.dumps(data, indent=4)
-
-# print(formatted_data)
-
