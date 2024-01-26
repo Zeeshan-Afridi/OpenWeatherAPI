887e8f33d5ec368f3671c4dc7d02ad1bfa330ae8
Zeeshan Afridi <80974922+Zeeshan-Afridi@users.noreply.github.com>
2024-01-26 15:11:38 +0500

    Create script.py
    Script added

---
+++ script.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+API_key = API
+city_name = "Peshawar"
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=metric"
+
+# url = f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_key}"
+
+
+respone = requests.get(url)
+
+data = respone.json()
+
+
+description = data['weather'][0]["description"]
+humidity = data["main"]["humidity"]
+temp_min = data['main']['temp_min']
+temp_max = data['main']["temp_max"]
+temp = data['main']["temp"]
+
+print(f"Maximum Temperature is: {temp_max} \nMinimum Temperature is: {temp_min} \nCurrent Temperature is: {temp}" +
+      f"\nCurrent Temperature Description is: {description} \nCurrent Temperature Humidity is: {humidity}")
+
+#Prettify the JSON data by specifying indent level
+# formatted_data = json.dumps(data, indent=4)
+
+# print(formatted_data)
+
